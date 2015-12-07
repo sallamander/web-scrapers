babc73e2d2f349f302cbc6fb9d100ac06cbe52c0
sallamander <ssall@alumni.nd.edu>
2015-12-06 18:56:32 -0700

    Rework album_of_the_year.py to use general_utilies

--- album_of_the_year.py
+++ album_of_the_year.py
@@ -1,20 +1,9 @@
-from bs4 import BeautifulSoup 
-import requests
+from general_utilities import get_html, select_soup
 
 if __name__ == '__main__':
-    response = requests.get('http://www.albumoftheyear.org/list/summary/2015/')
-    soup = BeautifulSoup(response.content, 'html.parser')
-    artist_titles = soup.select('.artistTitle')
-    album_titles = soup.select('.albumTitle')
-    album_points = soup.select('.summaryPoints')
-    album_places = soup.select('.summaryPointsMisc')
+    URL = 'http://www.albumoftheyear.org/list/summary/2015/'
+    soup = get_html(URL) 
 
-    artist_titles_contents = [soup.text for soup in artist_titles]
-    album_titles_contents = [soup.text for soup in album_titles]
-    album_points_contents = [soup.text for soup in album_points]
-    album_places_contents = [soup.text for soup in album_places]
+    css_selectors = ['.artistTitle', '.albumTitle', '.summaryPoints', '.summaryPointsMisc']
+    desired_contents = select_soup(soup, css_selectors)
 
-    # print artist_titles_contents
-    # print album_titles_contents
-    # print album_points_contents
-    print album_places_contents
