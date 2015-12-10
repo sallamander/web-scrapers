2d2d6279f3f38f92681b3ab1b3e452dcfd54df39
sallamander <ssall@alumni.nd.edu>
2015-12-10 12:27:10 -0700

    Rename variables in end_year_ablum_lists.py
    Variable names needed to be clearer and more descriptive before moving
    on to the next step and scraping each one of the individual critics
    pages.

--- end_year_album_lists.py
+++ end_year_album_lists.py
@@ -2,9 +2,11 @@ from general_utilities import get_html, select_soup, \
         output_data, grab_contents_key
 
 if __name__ == '__main__':
-    url = 'http://www.albumoftheyear.org/lists.php'
+    lists_url = 'http://www.albumoftheyear.org/lists.php'
 
-    soup = get_html(url)
-    desired_content = select_soup(soup, '.criticListBlockTitle')
-    desired_links = grab_contents_key(desired_content, 'a')
-    desired_hrefs = grab_contents_key(desired_links, 'href')
+    soup = get_html(lists_url)
+    critics_content = select_soup(soup, '.criticListBlockTitle')
+    critics_names = grab_contents_key(critics_content, "text")
+    critics_links = grab_contents_key(critics_content, 'a')
+    critics_hrefs = grab_contents_key(critics_links, 'href')
+
