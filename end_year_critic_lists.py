9f9f5556f6557e060664f56eef8500b300462597
sallamander <ssall@alumni.nd.edu>
2015-12-11 12:17:31 -0700

    Refactor end_year_critic_lists.py

--- end_year_critic_lists.py
+++ end_year_critic_lists.py
@@ -1,4 +1,5 @@
 from general_utilities import get_html, select_soup, grab_contents_key
+from albums_of_year_lst_ind import find_score
 from collections import defaultdict
 import re
 
@@ -12,50 +13,95 @@ def grab_critics_info(critics_names, critics_hrefs):
     where there is one dictionary for each critic list. 
     '''
 
-    base_individual_list_url = 'http://www.albumoftheyear.org'
     critics_hrefs_values = critics_hrefs.values()[0]
 
-    final_lst = []
-    css_selectors = ['.listLargeTitle']
     json_dct = defaultdict(list)
+    # Compile this here since we only need to do this once.
     regex = re.compile("^\d+\.")
+    # Cycle through each one of the critics. 
     for idx, critic_name in enumerate(critics_names.values()[0]): 
-
-        critic_url = base_individual_list_url + critics_hrefs_values[idx]
-        soup = get_html(base_individual_list_url + critics_hrefs_values[idx]) 
-        critic_list_content = select_soup(soup, css_selectors)
-        critic_lst_content_vals = critic_list_content.values()[0]
+        
+        critic_lst_content_vals, soup = \
+                get_critic_lst_content(critics_hrefs_values, idx)
         num_albums_idx = len(critic_lst_content_vals) 
-        critic_lst_content_vals.reverse()
+        # Cycle through each one of the ablums on the critics lists. 
         for idx2, values in enumerate(critic_lst_content_vals, 1):
+            # Each album is stored in a separate "Post-#". 
             post = soup.select('#post-' + str(num_albums_idx))[0]
-            album_title_text = post.select('.listLargeTitle')[0].text
-            critic_score_idx = post.text.find('Critic Score')
-            beg_idx, end_idx = critic_score_idx + 12, critic_score_idx + 14
-            critic_score = post.text[beg_idx:end_idx]
-            user_score_idx = post.text.find('User Score')
-            beg_idx, end_idx = user_score_idx + 10, user_score_idx + 12
-            user_score = post.text[beg_idx:end_idx]
-            value_text = values.text
-            rating = regex.findall(album_title_text)
-            if len(rating) >= 1: 
-                rating = int(rating[0].replace('.', ''))
-                album_title = album_title_text.split('-')[-1]\
-                        .encode('ascii', 'xmlcharrefreplace').strip()
-                critic_dct = {'Critic' : critic_name, 'Rating' : rating, 
-                        'User Score': user_score, 'Critic Score': critic_score}
-                json_dct[album_title].append(critic_dct)
-                num_albums_idx -= 1
-            else: 
-                rating = idx2
-                album_title = album_title_text.split('-')[-1]\
-                        .encode('ascii', 'xmlcharrefreplace').strip()
-                critic_dct = {'Critic' : critic_name, 'Rating' : rating, 
-                        'User Score': user_score, 'Critic Score': critic_score}
-                json_dct[album_title].append(critic_dct)
-                num_albums_idx -= 1
+
+            album_title, album_title_txt = get_album_title(post)            
+            critic_score = find_score(post, 'Critic Score') 
+            user_score = find_score(post, 'User Score')
+
+            rating_text = regex.findall(album_title_txt)
+            rating = parse_rating(rating_text, idx2)
+
+            critic_dct = {'Critic' : critic_name, 'Rating' : rating, 
+                    'User Score': user_score, 'Critic Score': critic_score}
+            json_dct[album_title].append(critic_dct)
+            num_albums_idx -= 1
 
     return json_dct 
+
+def get_critic_lst_content(critics_hrefs_values, idx):
+    '''
+    Input: List, Integer
+    Output: List, BeautifulSoup object
+
+    For the critic href at the inputted idx in the critics_hrefs_values, grab
+    all of the items with the class '.listLargeTitle'. This will then be used 
+    to cycle through each one of them and grab information from them. 
+    '''
+
+    base_individual_list_url = 'http://www.albumoftheyear.org'
+    css_selectors = ['.listLargeTitle']
+
+    critic_url = base_individual_list_url + critics_hrefs_values[idx]
+    soup = get_html(base_individual_list_url + critics_hrefs_values[idx]) 
+
+    critic_lst_content_vals = select_soup(soup, css_selectors).values()[0]
+    # We reverse them because they are posted from the highest ranked 
+    # (worst album) to the lowest rank (i.e. Post-1 is the highest ranked 
+    # album on the critic list).
+    critic_lst_content_vals.reverse()
+
+    return critic_lst_content_vals, soup
+
+def get_album_title(post): 
+    '''
+    Input: bs4.element.Tag
+    Output: String, String
+
+    Grab the album title from our inputted post. We are returning
+    the full text in addition to just the album title because the 
+    full text potentially has the rating in it. 
+    '''
+
+    album_title_txt = post.select('.listLargeTitle')[0].text
+    album_title = album_title_txt.split('-')[-1]\
+            .encode('ascii', 'xmlcharrefreplace').strip()
+
+    return album_title, album_title_txt
+
+def parse_rating(rating_txt, idx): 
+    '''
+    Input: String, Integer
+    Output: Integer
+
+    Get the rating for the current album that we are looking at on the critics
+    list. This falls into one of two scenarios: (1). Either the rating is 
+    already in the inputted rating_txt, and we just need to parse it, or (2). 
+    It's not there, and we need to assign it the inputted idx (this why we 
+    reversed the list in the get_critic_lst_content() function - the idx 
+    starts from one but the lists starts from the highest ranked (worst album). 
+    '''
+
+    if len(rating_txt) >= 1: 
+        rating = int(rating_txt[0].replace('.', ''))
+    else: 
+        rating = idx
+
+    return rating
 
 if __name__ == '__main__':
     lists_url = 'http://www.albumoftheyear.org/lists.php'
