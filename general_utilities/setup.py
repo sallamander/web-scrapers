e238be0f966d0c697646e55d86292ce95da5f3c4
sallamander <ssall@alumni.nd.edu>
2016-01-28 15:43:07 -0700

    Add setup file

---
+++ general_utilities/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='web_scrapers',
+      version='0.1',
+      py_modules=['query_utilities, storage_utilities'],
+      author='Sean Sall',
+      author_email='sean.sall@galvanize.com',
+)
