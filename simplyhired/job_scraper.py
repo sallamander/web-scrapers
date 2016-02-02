aa18f36f9ef65b729be8e8618d3696f487aca553
sallamander <ssall@alumni.nd.edu>
2016-02-02 13:13:35 -0700

    Set up skeleton for multiprocessing

--- simplyhired/job_scraper.py
+++ simplyhired/job_scraper.py
@@ -25,6 +25,28 @@ def parse_num_jobs_txt(num_jobs_txt):
     num_jobs = search_results[2].replace(',', '')
     return num_jobs
 
+def multiprocess_pages(base_URL, job_title, job_location, page_number): 
+    """Grab the URLS and other relevant info. from job postings on the page. 
+
+    The Simply Hired URL used for job searching takes another parameter, 
+    `pn`, that allows you to start the job search at jobs 11-20, 
+    21-30, etc. I can use this to grab job results from multiple pages at
+    once. This function takes in the base_URL and then adds that
+    pn={page_number} parameter to the URL, and then queries it. 
+    It passes the results on to a thread to grab the details from each
+    job posting.
+
+    Args: 
+        base_URL: String that holds the base URL to add the page_start 
+            parameter to. 
+        job_title: String holding the job title used for the search
+        job_location: String holding the job location used for the search 
+        page_number: Integer of what the `start` parameter in the URL should
+            be set to. 
+    """
+
+    pass
+
 if __name__ == '__main__':
     # I expect that at the very least a job title, job location, and radius
     # will be passed in, so I'll attempt to get both of those within
@@ -47,3 +69,12 @@ if __name__ == '__main__':
     html = get_html(query_URL)
     num_jobs_txt = str(html.select('.result-headline')[0].text)
     num_jobs = int(parse_num_jobs_txt(num_jobs_txt))
+    # Now we need to cycle through all of the job postings that we can 
+    # and grab the url pointing to it, to then query it. All of the jobs
+    # should be available through the '.js-job-link' class.
+    max_pages = num_jobs / 10 + 1
+    page_numbers = range(1, max_pages + 1)
+    execute_queries = partial(multiprocess_pages, query_URL, \
+            job_title, job_location)
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    pool.map(execute_queries, page_numbers)
