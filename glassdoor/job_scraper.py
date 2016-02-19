78136974dbeb8d2151d0e17c87af03caf590a7ab
sallamander <ssall@alumni.nd.edu>
2016-02-19 15:33:28 -0700

    Add function to parse the total number of pages

--- glassdoor/job_scraper.py
+++ glassdoor/job_scraper.py
@@ -52,6 +52,27 @@ def parse_num_jobs_txt(num_jobs_txt):
 
     return num_jobs
 
+def parse_num_pages_txt(num_pages_txt): 
+    """Parse the text that holds the number of pages to see how many pages
+    we have to iterate over. 
+
+    This will use a regex to find the number of pages of job search results 
+    that we obtained in our job search. The number of pages can be found by 
+    parsing the text telling us what page of the total we are on (i.e. 
+    Page 1 of 10, or something like that). 
+
+    Args: 
+        num_pages_txt: Str
+            Contains the text that holds the total number of pages that
+            are returned from our job search. 
+    """
+
+    regex = re.compile('\d*[,]?\d+[+]*')
+    search_results = re.findall(regex, num_jobs_txt)
+    num_pages = search_results[1].replace(',', '')
+
+    return num_pages
+
 if __name__ == '__main__':
     # I expect that at the very least a job title and job location
     # will be passed in, so I'll attempt to get both of those within
@@ -73,8 +94,10 @@ if __name__ == '__main__':
 
     # Find the text holding the number of jobs, and parse it. 
     time.sleep(random.randint(7, 15))
-    headers = driver.find_elements_by_xpath('//header')
-    num_jobs_txt = headers[1].text
+    num_jobs_txt = driver.find_elements_by_xpath('//header')[1].text
     num_jobs = parse_num_jobs_txt(num_jobs_txt) 
-
-    driver.close()
+    
+    # Find the text holding the number of pages in the job search. 
+    time.sleep(random.randint(2, 6))
+    num_pages_txt = driver.find_element_by_id('ResultsFooter').text
+    num_pages = parse_num_pages_txt(num_pages_txt)
