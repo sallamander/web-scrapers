4227167bf9182105963a8f865256b009e47fae5c
sallamander <ssall@alumni.nd.edu>
2016-02-20 21:42:16 -0700

    Add code to accept user input

---
+++ careerbuilder/job_scraper.py
@@ -0,0 +1,12 @@
+
+if __name__ == '__main__':
+    # I expect that at the very least a job title and job location
+    # will be passed in, so I'll attempt to get both of those within
+    # a try except and throw an error otherwise. 
+    try: 
+        job_title = sys.argv[1]
+        job_location = sys.argv[2]
+    except IndexError: 
+        raise Exception('Program needs a job title and job location inputted!')
+
+
