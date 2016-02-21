8c785fcd6f89e1ec1e78539a0e50566dafb52a98
sallamander <ssall@alumni.nd.edu>
2016-02-21 08:26:41 -0700

    Add code to check status codes of responses

--- general_utilities/query_utilities.py
+++ general_utilities/query_utilities.py
@@ -19,17 +19,37 @@ def format_query(base_url, query_parameters):
 
 def get_html(url): 
     '''
-    Input: String
-    Output: HTML content 
-
     Take in a URL, issue a get request on that HTML, and then return the 
     parsed content from the response using BeautifulSoup. 
+
+    Args: 
+        url: str
+            Holds the URL to get the HTML from.
     '''
 
     try: 
         response = requests.get(url)
+        good_response = check_response_code(response)
+        if not good_response: 
+            # I want to be able to check the bad_url to see what happened.
+            print url
         soup = BeautifulSoup(response.content, 'html.parser')
         return soup
     except: 
         error = "Error in contacting the URL - check that it is a valid URL!"
         raise RuntimeError(error)
+
+def check_response_code(response): 
+    """Check response status code. 
+
+    Args: 
+        response: requests.models.Response
+            Holds the response from issuing a `get` request. 
+    """
+
+    status_code = response.status_code
+    if status_code == 200: 
+        return True
+    else: 
+        print "Status code is not 200, it's {}".format(status_code)
+        return False
