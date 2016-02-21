5b5cedd4033f6b10f165beeba854b0d17e24ef92
sallamander <ssall@alumni.nd.edu>
2016-02-20 21:51:34 -0700

    Fill in issue_query function

--- careerbuilder/job_scraper.py
+++ careerbuilder/job_scraper.py
@@ -1,9 +1,30 @@
+import sys
+import time
+import random
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 def issue_query(driver, job_title, job_location): 
+    """Issue the initial job search query so we can start scraping. 
+
+    Here, we'll search for the input boxes through which we need to 
+    input a job title and job location. We'll input a job title and job
+    location, and then hit the enter button to perform the search.
+
+    Args: 
+        driver: Selenium webdriver
+        job_title: str
+            A string holding the job title we're using in the search. 
+        job_location: str
+            A string holding the job location we're using in the search.
     """
-    """
-    pass
+
+    title_search = driver.find_element_by_id('search-key')
+    location_search = driver.find_element_by_id('search-loc')
+
+    title_search.send_keys(job_title)
+    location_search.send_keys(job_location)
+    location_search.send_keys(Keys.ENTER)
 
 if __name__ == '__main__':
     # I expect that at the very least a job title and job location
