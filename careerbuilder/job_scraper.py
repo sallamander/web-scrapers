48e1278bc47207e2286acac55df4c8d28eb063f0
sallamander <ssall@alumni.nd.edu>
2016-02-23 22:33:16 -0700

    Fill in scrape_job_page

--- careerbuilder/job_scraper.py
+++ careerbuilder/job_scraper.py
@@ -4,18 +4,49 @@ wd = os.path.abspath('.')
 sys.path.append(wd + '/../')
 import time
 import random
+import datetime
 from general_utilities.navigation_utilities import issue_driver_query
 from general_utilities.parsing_utilities import parse_num
 
 def scrape_job_page(driver, job_title, job_location): 
+    """Scape a page of jobs from CareerBuilder.
+
+    We will grab all the relevant information that we can for each of 
+    the jobs posted on a given page. This will include the job title, 
+    job location, posting company, and date posted. 
+
+    Args: 
+        driver: Selenium webdriver
+        job_title: str
+            String holding the job title we searched for. 
+        job_location: str
+            String holding the job location we searched for. 
     """
+
+    titles, locations, companies, dates, hrefs = query_for_data()
+
+    current_date = datetime.date.today().strftime("%m-%d-%Y")
+    json_dct = {'search_title': job_title, \
+            'search_location': job_location, \
+            'search_date': current_date}
+
+    jobs = driver.find_elements_by_class_name('gs-job-result-abstract')
+    return jobs
+
+def query_for_data(): 
+    """Grab all the relevant data on a jobs page. 
+    
+    For each page of jobs, we will ultimately want to grab each of 
+    the jobs title, location, posting company, and dates posted. Finally, 
+    we'll want to grab the href of the job posting, and use that to get 
+    the all of the jobs posting. 
     """
     pass
-
+    
 def check_if_next(driver):
     """
     """
-    pass
+    return False 
 
 if __name__ == '__main__':
     # I expect that at the very least a job title and job location
@@ -41,5 +72,5 @@ if __name__ == '__main__':
     # scraping jobs on that page. 
     is_next = True
     while is_next: 
-        scrape_job_page(driver, job_title, job_location)
+        jobs = scrape_job_page(driver, job_title, job_location)
         is_next = check_if_next(driver)
