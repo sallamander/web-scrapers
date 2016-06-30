db58199b6777e1f6f99ed8a1b20d5d385471aed8
sallamander <ssall@alumni.nd.edu>
2016-06-30 05:58:47 -0500

    Add function for scraping pages within a time interval

--- nytimes/article_scraper.py
+++ nytimes/article_scraper.py
@@ -5,10 +5,46 @@ import os
 wd = os.path.abspath('.')
 sys.path.append(wd + '/../')
 from requests import get
+from time import sleep
 from general_utilities.query_utilities import check_response_code
 
-def issue_single_query(api_key, start_dt, end_dt, page=0): 
-    """Issue a query on the NYTimes API given the inputted parameters. 
+def scrape_nyt(api_key, start_dt, end_dt): 
+    """Scrape the NYTimes for all results within an inputted start and end date.
+
+    Issue a single query for the inputted date range, and then cycle through all
+    pages of results that are returned, if there are mutiple pages (each page stores
+    10 results, so if there are more than 10 results, there is more than 1 page).
+
+    Args:
+    ----
+        api_key: str
+        start_dt: str
+        end_dt: str
+
+    Return: 
+    ------
+        article_urls: list
+    """
+    
+    article_urls = []
+    initial_response = scrape_single_page(api_key, start_dt, end_dt)
+    num_results = initial_response['response']['meta']['hits']
+
+    article_urls = [doc['web_url'] for doc in initial_response['response']['docs']]
+    if num_results > 10: 
+        # They don't allow querying past the 100th page. 
+        max_pages_to_search = min(100, num_results // 10)
+        for page in range(1, max_pages_to_search): 
+            sleep(1/5) # Use to avoid hitting the rate limit. 
+            response = scrape_single_page(api_key, start_dt, end_dt, page=page)
+            article_urls = add_page_urls(article_urls, response)
+
+    return article_urls
+
+def scrape_single_page(api_key, start_dt, end_dt, page=0): 
+    """Issue a query against a single page of the NYTimes API for the inputted dates.
+
+    This will largely be called as a helper function to `scrape_nyt`. 
 
     Args:
     ----
@@ -38,6 +74,25 @@ def issue_single_query(api_key, start_dt, end_dt, page=0):
 
     return response.json()
 
+def add_page_urls(article_urls, response): 
+    """Append any article URL's found in the inputted reponse to the `article_urls`.
+
+    Args: 
+    ----
+        article_urls: list
+        response: requests.models.Response object
+            Expected to have a 'response' attribute, with a 'docs' attribute. 
+
+    Return: 
+    ------
+        article_urls: list
+    """
+
+    for doc in response['response']['docs']: 
+        article_urls.append(doc['web_url'])
+
+    return article_urls
+
 if __name__ == '__main__':
     try: 
         start_dt = sys.argv[1]
@@ -46,5 +101,5 @@ if __name__ == '__main__':
         raise Exception("Usage: python article_scraper.py start_dt end_dt")
 
     api_key = os.environ['NYTIMES_API_KEY']
-    response = issue_single_query(api_key, start_dt, end_dt)
+    article_urls = scrape_nyt(api_key, start_dt, end_dt)
 
